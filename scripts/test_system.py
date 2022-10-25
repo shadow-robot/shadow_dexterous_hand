5dad3d9fdbd63f8f27f80338a76371c143a78755
Ben Starmer-Smith <ben@shadowrobot.com>
2022-10-25 09:42:53 +0100

    Fixed the linter.

--- scripts/test_system.py
+++ scripts/test_system.py
@@ -18,10 +18,11 @@ import time
 import subprocess
 import rospy
 
+
 class TestSystem(unittest.TestCase):
 
     def test_roscore(self):
-        for i in range(0,5):
+        for i in range(0, 5):
             time.sleep(5)
             try:
                 rospy.get_master().getPid()
@@ -32,6 +33,7 @@ class TestSystem(unittest.TestCase):
                 if i < 4:
                     subprocess.call('echo "Can\'t connect to roscore, remaing retries: %d"' % (4-i), shell=True)
         self.assertEqual(status, 0, "roscore is not running")
+
 
 if __name__ == "__main__":
     rospy.init_node('system_test_node', anonymous=True)
